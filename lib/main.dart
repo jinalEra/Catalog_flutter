2da11dc5ddc100c2caa75665bf0f38c409202982
Jinal Era <jinalpanchal4277@gmail.com>
2021-07-03 20:01:29 +0530

    Day-3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:catalog/home/homepage.dart';
+import 'package:catalog/Screens/home/homepage.dart';
+import 'package:catalog/Screens/home/loginpage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+
+      // home: HomePage(),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login" : (context) => Loginpage(),
+      },
     );
   }
 }
